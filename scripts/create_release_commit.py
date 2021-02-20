a46434cd53e0abd11485bd97d84fd7ce605ae8eb
Jovyan123 <jovyan123.playground@gmail.com>
2021-02-20 08:27:53 -0600

    Publish 1.5.0
    SHA256 hashes:
    
    jupyter_server-1.5.0.dev0.tar.gz: 83f53aa3485a6fff3d042e0b5a06aeee7435bbeee603a82e077ec9f9e84e6142
    
    jupyter_server-1.5.0.dev0-py3-none-any.whl: 0a419926de4ae1e73ac2430c2903f4d8060f2a90ce18515c5192323c6cb52037

--- scripts/create_release_commit.py
+++ scripts/create_release_commit.py
@@ -1,43 +1,47 @@
+import argparse
 import hashlib
 import os
 import shlex
 import subprocess
-
-version = "1.5.0"
+import sys
 
 BUF_SIZE = 65536
 
-cmd = f'git commit -am "Publish {version}" -m "SHA256 hashes:"'
+DESCRIPTION = "Generate a git commit for a release."
+parser = argparse.ArgumentParser(description=DESCRIPTION)
+parser.add_argument(
+    "version",
+    help="""The new version.""",
+)
 
-for fname in os.listdir('dist'):
-    sha256 = hashlib.sha256()
+def main():
+    args = parser.parse_args(sys.argv[1:])
+    cmd = f'git commit -am "Publish {args.version}" -m "SHA256 hashes:"'
+    files = os.listdir('dist')
+    if not len(files) == 2:
+        raise ValueError('Missing distribution files')
 
-    with open(os.path.join('dist', fname), 'rb') as f:
-        while True:
-            data = f.read(BUF_SIZE)
-            if not data:
-                break
-            sha256.update(data)
-    sha256 = sha256.hexdigest()
-    print(fname, sha256)
-    cmd += f' -m "{fname}: {sha256}"'
+    for fname in files:
+        if not args.version in fname:
+            raise ValueError('Wrong version in distribution files')
 
-print(cmd)
-subprocess.run(shlex.split(cmd))
-# const files = fs.readdirSync(distDir);
-# const hashes = new Map<string, string>();
-# files.forEach(file => {
-#     const shasum = crypto.createHash('sha256');
-#     const hash = shasum.update(fs.readFileSync(path.join(distDir, file)));
-#     hashes.set(file, hash.digest('hex'));
-# });
+        sha256 = hashlib.sha256()
 
-# const hashString = Array.from(hashes.entries())
-#     .map(entry => `${entry[0]}: ${entry[1]}`)
-#     .join('" -m "');
+        with open(os.path.join('dist', fname), 'rb') as f:
+            while True:
+                data = f.read(BUF_SIZE)
+                if not data:
+                    break
+                sha256.update(data)
+        sha256 = sha256.hexdigest()
+        print(fname, sha256)
+        cmd += f' -m "{fname}: {sha256}"'
 
-# // Make the commit and the tag.
-# const curr = utils.getPythonVersion();
-# utils.run(
-#     `git commit -am "Publish ${curr}" -m "SHA256 hashes:" -m "${hashString}"`
-# );
+    print(cmd)
+    subprocess.run(shlex.split(cmd))
+
+
+if __name__ == "__main__":
+    main()
+
+
