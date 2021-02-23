c58ac7abf4dabe414ae3480a5feae67ee93baba1
Jovyan123 <jovyan123.playground@gmail.com>
2021-02-23 08:04:28 -0600

    test pypi upload

--- scripts/release_prep.sh
+++ scripts/release_prep.sh
@@ -36,14 +36,14 @@ twine check dist/*
 NAME=$(python setup.py --name)
 virtualenv -p $(which python3) test_sdist
 fname=$(ls dist/*.tar.gz)
-./test_sdist/bin/pip install -q ${fname}[test]
-./test_sdist/bin/pytest --pyargs "${NAME}"
+#./test_sdist/bin/pip install -q ${fname}[test]
+#./test_sdist/bin/pytest --pyargs "${NAME}"
 
 # Test wheel in venv
 virtualenv -p $(which python3) test_wheel
 fname=$(ls dist/*.whl)
-./test_wheel/bin/pip install -q ${fname}[test]
-./test_wheel/bin/pytest --pyargs "${NAME}"
+#./test_wheel/bin/pip install -q ${fname}[test]
+#./test_wheel/bin/pytest --pyargs "${NAME}"
 
 # Create the commit with shas
 python scripts/create_release_commit.py
@@ -60,7 +60,7 @@ fi
 # Test push to PyPI
 # TODO: need to get a TEST_PYPI_API_TOKEN from 
 # https://test.pypi.org/account/register/
-# twine upload -u github_bot --repository-url https://test.pypi.org/legacy/ dist/*
+twine upload -u __token__ password=${TEST_PYPI_TOKEN} --repository-url https://test.pypi.org/legacy/ dist/*
 
 # Verify the commits and tags
 git --no-pager diff HEAD ${FULL_BRANCH} > diff.diff
