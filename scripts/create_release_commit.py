0746a1816a9eb6051a2778cc121bcbf6889876be
Jovyan123 <jovyan123.playground@gmail.com>
2021-02-23 07:35:05 -0600

    debug

--- scripts/create_release_commit.py
+++ scripts/create_release_commit.py
@@ -29,7 +29,7 @@ def main():
 
     for fname in files:
         if not str(args.version) in fname:
-            print(fname, args.version)
+            print(repr(fname), repr(args.version))
             raise ValueError('Wrong version in distribution files')
 
         sha256 = hashlib.sha256()
