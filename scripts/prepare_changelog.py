6ac8e5b8fb2b16d652d9372b2cd14836ad841821
Jovyan123 <jovyan123.playground@gmail.com>
2021-02-23 05:00:01 -0600

    remove decode

--- scripts/prepare_changelog.py
+++ scripts/prepare_changelog.py
@@ -97,7 +97,7 @@ def get_changelog_entry(target, branch, version, auth=None, resolve_backports=Fa
         A formatted changelog entry with markers
     """
     since = run(f'git tag --merged origin/{branch}')
-    since = since.decode('utf-8').splitlines()[-1]
+    since = since.splitlines()[-1]
     print(f'Getting changes since {since}...')
 
     md = generate_activity_md(
