ed8a1b6824b417a16230dabcd272844633b2862e
Jovyan123 <jovyan123.playground@gmail.com>
2021-02-23 05:30:45 -0600

    parameterize more things

--- scripts/changelog_prep.sh
+++ scripts/changelog_prep.sh
@@ -2,8 +2,9 @@
 set -ex
 
 # Fetch the target branch
-git remote add upstream https://github.com/jupyter-server/jupyter_server
+git remote add upstream https://github.com/${TARGET}
 git fetch upstream ${BRANCH} --tags
+BRANCH="upstream/${BRANCH}"
 
 ## Install package with packaging deps
 pip install -e .[packaging]
@@ -12,7 +13,7 @@ pip install -e .[packaging]
 tbump --non-interactive --only-patch ${VERSION}
 
 ## Prepare the changelog
-python scripts/prepare_changelog.py "jupyter-server/jupyter_server" CHANGELOG.md --branch "upstream/${BRANCH}"
+python scripts/prepare_changelog.py ${TARGET} ${CHANGELOG} --branch ${BRANCH}
 
 ## Commit the changelog
 git add CHANGELOG.md
@@ -24,7 +25,7 @@ git checkout .
 ## Verify the change for the PR
 git diff --numstat | wc -l | grep "0"
 git diff --numstat HEAD~1 HEAD | grep "1"
-git --no-pager diff HEAD upstream/${BRANCH} > diff.diff
+git --no-pager diff HEAD ${BRANCH} > diff.diff
 cat diff.diff
 cat diff.diff > grep "# ${VERSION}"
 
--- scripts/release_prep.sh
+++ scripts/release_prep.sh
@@ -2,8 +2,9 @@
 set -ex
 
 # Fetch the target branch
-git remote add upstream https://github.com/jupyter-server/jupyter_server
+git remote add upstream https://github.com/{$TARGET}
 git fetch upstream ${BRANCH} --tags
+BRANCH="upstream/${BRANCH}"
 
 ## Install package with packaging deps
 pip install -e .[packaging]
@@ -12,7 +13,7 @@ pip install -e .[packaging]
 tbump --non-interactive --only-patch ${VERSION}
 
 # Finalize the changelog and write changelog entry file
-python scripts/finalize_changelog.py "jupyter-server/jupyter_server" CHANGELOG.md --branch "upstream/${BRANCH}" -o ${CHANGELOG_OUTPUT}
+python scripts/finalize_changelog.py ${TARGET} ${CHANGLOG} --branch ${BRANCH} -o ${CHANGELOG_OUTPUT}
 
 # Build and check the dist files
 rm -f dist
@@ -48,7 +49,7 @@ fi
 twine upload --repository-url https://test.pypi.org/legacy/ dist/*
 
 # Verify the commits and tags
-git --no-pager diff HEAD upstream/${BRANCH} > diff.diff
+git --no-pager diff HEAD ${BRANCH} > diff.diff
 cat diff.diff | grep ${VERSION}
 if [ -n ${POST_VERSION} ]; then
     cat diff.dif | grep ${POST_VERSION}
