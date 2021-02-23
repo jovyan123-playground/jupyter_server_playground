cdeb86168f9f6c952401e0225a9350a2a5841e69
Jovyan123 <jovyan123.playground@gmail.com>
2021-02-23 08:14:37 -0600

    add handling of changelog pr in verification and turn back on tests

--- scripts/release_prep.sh
+++ scripts/release_prep.sh
@@ -36,14 +36,14 @@ twine check dist/*
 NAME=$(python setup.py --name)
 virtualenv -p $(which python3) test_sdist
 fname=$(ls dist/*.tar.gz)
-#./test_sdist/bin/pip install -q ${fname}[test]
-#./test_sdist/bin/pytest --pyargs "${NAME}"
+./test_sdist/bin/pip install -q ${fname}[test]
+./test_sdist/bin/pytest --pyargs "${NAME}"
 
 # Test wheel in venv
 virtualenv -p $(which python3) test_wheel
 fname=$(ls dist/*.whl)
-#./test_wheel/bin/pip install -q ${fname}[test]
-#./test_wheel/bin/pytest --pyargs "${NAME}"
+./test_wheel/bin/pip install -q ${fname}[test]
+./test_wheel/bin/pytest --pyargs "${NAME}"
 
 # Create the commit with shas
 python scripts/create_release_commit.py
