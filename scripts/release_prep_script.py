0cf0c1a9ce67ab0f67942e98235ec1db67205eae
Jovyan123 <jovyan123.playground@gmail.com>
2021-02-25 07:25:14 -0600

    fix variable error and add todo

--- scripts/release_prep_script.py
+++ scripts/release_prep_script.py
@@ -130,6 +130,8 @@ if __name__ == "__main__":
 
 # TODO
 #- test the full workflow against the playground and the test pypi server
+#- refactor out the actions into changelog_core, release_core, release_test, changelog, and release
+#  to save duplication
 # on: 
 #   workflow_dispatch:
 #     inputs:
