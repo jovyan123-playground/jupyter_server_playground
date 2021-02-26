d94448a67b829e5dac8f650c01bcd1a1516664a7
Jovyan123 <jovyan123.playground@gmail.com>
2021-02-26 05:19:53 -0600

    add create changelog

--- scripts/release_prep_script_node.py
+++ scripts/release_prep_script_node.py
@@ -115,17 +115,3 @@ def main(args):
 if __name__ == "__main__":
     args = parser.parse_args(sys.argv[1:])
     main(args)
-
-# TODO
-#- test the full workflow against the playground and the test pypi server
-#- refactor out the actions into changelog_core, release_core, release_test, changelog, and release
-#  to save duplication
-# on: 
-#   workflow_dispatch:
-#     inputs:
-#       logLevel:
-#         description: 'Log level'     
-#         required: true
-#         default: 'warning'
-#       tags:
-#         description: 'Test scenario tags' 
