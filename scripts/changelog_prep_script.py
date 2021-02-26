7eb4b6d622b166963c4f3cce04dfcb4c3df7d42f
Jovyan123 <jovyan123.playground@gmail.com>
2021-02-26 09:04:18 -0600

    cleanup

--- scripts/changelog_prep_script.py
+++ scripts/changelog_prep_script.py
@@ -17,14 +17,6 @@ parser.add_argument(
     default="tbump --non-interactive --only-patch",
     help="""The version command to run.""",
 )
-parser.add_argument(
-    "--postprocess-command", "-pc",
-    help="""The command to run to postprocess the changelog entry.""",
-)
-parser.add_argument(
-    "--output", "-o",
-    help="""The output path to store the new change entry if needed.""",
-)
 
 
 def main(args):
@@ -34,7 +26,6 @@ def main(args):
     version_command = args.version_command
     postprocess_command = args.postprocess_command
     changelog = args.file
-    output_file = args.output
 
     ## Bump the verison
     run(f'{version_command} {version_spec}')
@@ -46,11 +37,7 @@ def main(args):
     run('git checkout .')
 
     ## Prepare the changelog
-    new_entry = prepare_changelog(args)
-
-    # Run the pre release command if given
-    if postprocess_command:
-        run(postprocess_command)
+    prepare_changelog(args)
 
     ## Verify the change for the PR
     # Only one uncommitted file
@@ -58,10 +45,6 @@ def main(args):
     # New version entry in the diff
     diff = run(f'git --no-pager diff')
     assert f"# {version}" in diff
-
-    if output_file:
-        with open(output_file, 'w') as fid:
-            fid.write(new_entry)
 
     # Follow up actions
     print("Changelog Prep Complete!")
