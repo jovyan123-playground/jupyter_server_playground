f6ec0723659cd4118c809012d979bae15e1acd5e
Jovyan 123 <jovyan123.playground@gmail.com>
2021-03-18 04:10:46 -0500

    fix one more part

--- examples/simple/simple_ext1/handlers.py
+++ examples/simple/simple_ext1/handlers.py
@@ -21,7 +21,7 @@ class ParameterHandler(ExtensionHandlerMixin, JupyterHandler):
         var1 = self.get_argument('var1', default=None)
         components = [x for x in self.request.path.split("/") if x]
         self.write('<h1>Hello Simple App 1 from Handler.</h1>')
-        self.write('<p>matched_part: {}</p>'.format(matched_part))
+        self.write('<p>matched_part: {}</p>'.format(url_escape(matched_part)))
         self.write('<p>var1: {}</p>'.format(url_escape(var1)))
         self.write('<p>components: {}</p>'.format(components))
 
