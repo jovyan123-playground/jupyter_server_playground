cf06a7ac8261a471bcf37bdfe01e482376d29ae4
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2021-04-01 06:49:07 -0500

    wip

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 from setuptools import setup
 
 try:
-    from jupyter_packaging import create_cmdclass, install_npm
-    cmdclass = create_cmdclass('jsdeps')
-    cmdclass['jsdeps'] = install_npm()
+    from jupyter_packaging import wrap_installers, npm_builder
+    cmdclass = wrap_installers(pre_develop=npm_builder())
 except ImportError:
     cmdclass = {}
 
