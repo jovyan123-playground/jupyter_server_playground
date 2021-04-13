145ee628da0d48f090c1014d307543f266455f98
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2021-04-13 04:48:42 -0500

    use get_version_info

--- jupyter_server/_version.py
+++ jupyter_server/_version.py
@@ -2,14 +2,8 @@
 store the current version info of the server.
 
 """
-from pkg_resources import parse_version
+from jupyter_packaging import get_version_info
 
 # Version string must appear intact for tbump versioning
 __version__ = '1.7.0.dev0'
-
-# Build up the version info tuple from the version string
-parsed = parse_version(__version__)
-version_info = [parsed.major, parsed.minor, parsed.micro]
-if parsed.base_version != parsed.public:
-    version_info.append(parsed.public[len(parsed.base_version):])
-version_info = tuple(version_info)
+version_info = get_version_info(__version__)
